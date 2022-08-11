4cab4c98579c4a0e35a453d9357342501783686b
Patrick Morrison <pmorrisonfl@gmail.com>
2022-08-11 17:00:07 -0400

    updated read1 grader

--- read1/grader.py
+++ read1/grader.py
@@ -9,4 +9,4 @@ def exists():
 @check50.check(exists)
 def file_contains_netid():
     """Check whether answer.txt contains correct string"""
-    check50.run("cat answer.txt").stdout("a is {0, 3, 6, 30, 24}\n*p is 6\n", regex=False)
+    check50.run("cat answer.txt").stdout("a is 3\nb is 8\na * i + b = 8\na * i + b = 12\na * i + b = 16\na * i + b = 20\na is 5\nb is 6\n", regex=False)
